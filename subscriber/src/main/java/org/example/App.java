9c846707d84e121412d4027765e7cb86b86a7c8f
Stefcool8 <stefan.nastasiu8@gmail.com>
2025-06-13 17:57:07 +0300

    main: First commit. Added 2 modules: publisher & subscriber. More modules can be added and run separately.

---
+++ subscriber/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        for (int i = 0; i < 100000000; i++) {
+            System.out.println("Hello World! " + i);
+        }
+    }
+}
